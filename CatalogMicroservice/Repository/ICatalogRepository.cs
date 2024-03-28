1aadb00a57ac5ad08a22a1fcebff67030d234130
Haroldo <haroldovinente@gmail.com>
2024-03-28 14:14:15 -0300

    Iniciando projeto, repositoriso do microserviço de catalog

---
+++ CatalogMicroservice/Repository/ICatalogRepository.cs
@@ -0,0 +1,12 @@
+using CatalogMicroservice.Model;
+
+namespace CatalogMicroservice.Repository;
+
+public interface ICatalogRepository
+{
+    IList<CatalogItem> GetCatalogItems();
+    CatalogItem? GetCatalogItem(string catalogItemId);
+    void InsertCatalogItem(CatalogItem catalogItem);
+    void UpdateCatalogItem(CatalogItem catalogItem);
+    void DeleteCatalalogItem(string catalogItemId);
+}
